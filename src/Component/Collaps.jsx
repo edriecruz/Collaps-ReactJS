5769e67744f5f63f885a7abce4b4ecbd4a0a09cd
Edrie Isaac Cruz <eddsycruz@gmail.com>
2022-05-03 17:59:54 +0800

    first commit

--- src/Component/Collaps.jsx
+++ src/Component/Collaps.jsx
@@ -9,13 +9,13 @@ export function Collaps(props) {
     console.log(parentRef.current.scrollHeight);
 
     return (
-        <div className='collaps ml-10 mt-10'>
+        <div className='collaps ml-10 mt-10 flex flex-col items-center'>
           <button 
-          className='toggle shadow-2xl p-6 w-52 rounded-t-md' 
+          className='toggle shadow-2xl p-6 w-52 rounded-xl bg-rose-100 transition delay-150 duration-300 ease-in-out hover:scale-110' 
           onClick={() => setOpen(!open)}>
             {props.label}
           </button>
-          <div className='content-parent' 
+          <div className='content-parent mt-3 rounded-lg' 
           ref={parentRef} 
           style={
             open 
--- src/Component/Content.jsx
+++ src/Component/Content.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import { Collaps } from './Collaps';
+import deadrose from '../assets/deadrose.jpg'
 
 export function Content(){
 
 return(
-    <div className='flex flex-col md:flex-row justify-center mt-10 mx-5'>
-    <Collaps label="Hello" className=''>
-        <div className=' bg-slate-200 w-52 flex justify-center rounded-b-2xl p-3 shadow-2xl'>
+    <div className='relative flex flex-col md:flex-row justify-around h-screen w-screen'
+    style={{background: 'url('+ deadrose +')', backgroundPositionY: '-25rem'}}>
+        <Collaps label="Hello" className=''>
+        <div className=' bg-slate-200 w-60 flex justify-center p-3 shadow-2xl transition delay-150 duration-300 ease-in-out'>
             I'm Edrie Cruz, living in Taytay, Rizal and I'm studying Information and Communication Technology aiming to be a Software Developer where I can improve myself through coding and programming and hone my skills through experience.
         </div>
     </Collaps>
 
     <Collaps label="Achievements" className=''>
-        <div className=' bg-slate-200 w-52 flex justify-center rounded-b-2xl p-3 shadow-2xl'>
+        <div className=' bg-slate-200 w-60 flex justify-center p-3 shadow-2xl transition delay-150 duration-300 ease-in-out'>
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
         </div>
     </Collaps>
 
     <Collaps label="Hobbies" className=''>
-        <div className=' bg-slate-200 w-52 flex flex-col space-y-5 justify-center rounded-b-2xl p-3 shadow-2xl'>
+        <div className=' bg-slate-200 w-60 flex flex-col space-y-5 justify-center p-3 transition delay-150 duration-300 ease-in-out'>
             <h1>~ Work Out 💪🏼</h1>
             <h1>~ Playing Games 🕹</h1>
             <h1>~ Coding Stuff 💻</h1>
             <p>etc.</p>
         </div>
     </Collaps>
-
+    
     </div>
 );
 }
